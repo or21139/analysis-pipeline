33bf4b2f475d7e7da5b31e3b1db0d3767938682e
or21139 <or21139@bristol.ac.uk>
2025-12-15 12:38:48 +0000

    Update msa_cleaner.sh
    added comment that was missed

--- msa_cleaner.sh
+++ msa_cleaner.sh
@@ -4,6 +4,7 @@ mapfile -t organism_code < <(grep -oP '^>[^ ]*' $1) #create an array of the acce
 mapfile -t other_header_info < <(grep -oP '[0-9]+\-[0-9]+\K.*' $1) #create an array of the rest of each header
 other_header_info=("" "${other_header_info[@]}")
 
+#add the header info to each header line
 for index in ${!organism_code[@]}; do
 	sed -i -e "s#${organism_code[index]}#${organism_code[index]}${other_header_info[index]}#" $2
 done
